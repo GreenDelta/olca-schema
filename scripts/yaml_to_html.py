3249649c7309012c84ed3d1b1e8d6aec07eeb54f
msrocka <michael.srocka@gmail.com>
2014-11-04 20:40:49 +0100

    ttl generation of enums and classes

--- scripts/yaml_to_html.py
+++ scripts/yaml_to_html.py
@@ -5,21 +5,32 @@ Generates the HTML documentation from the YAML model.
 import model
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('./templates'))
 
-class_template = env.get_template('class_template.html')
-enum_template = env.get_template('enum_template.html')
-m = model.Model.load_yaml('../yaml')
+def main():
+    env = Environment(loader=FileSystemLoader('./templates'))
+    class_template = env.get_template('class_template.html')
+    enum_template = env.get_template('enum_template.html')
+    m = model.Model.load_yaml('../yaml')
+    for t in m.types:
+        if type(t) == model.ClassType:
+            write_class(class_template, m, t)
+        if type(t) == model.EnumType:
+            write_enum(enum_template, t)
 
-for t in m.types:
-    if type(t) == model.ClassType:
-        super_classes = m.get_super_classes(t)
-        text = class_template.render(model=t, super_classes=super_classes)
-        file_path = '../html/%s.html' % t.name
-        with open(file_path, 'w') as f:
-            f.write(text)
-    if type(t) == model.EnumType:
-        text = enum_template.render(model=t)
-        file_path = '../html/%s.html' % t.name
-        with open(file_path, 'w') as f:
-            f.write(text)
+
+def write_enum(template, t):
+    text = template.render(model=t)
+    file_path = '../html/%s.html' % t.name
+    with open(file_path, 'w') as f:
+        f.write(text)
+
+
+def write_class(template, m, t):
+    super_classes = m.get_super_classes(t)
+    text = template.render(model=t, super_classes=super_classes)
+    file_path = '../html/%s.html' % t.name
+    with open(file_path, 'w') as f:
+        f.write(text)
+
+if __name__ == '__main__':
+    main()
--- scripts/yaml_to_ttl.py
+++ scripts/yaml_to_ttl.py
@@ -1,5 +1,41 @@
 
+from io import StringIO
 import model
+
+
+def main():
+    buffer = StringIO()
+    buffer.write(get_header())
+    m = model.Model.load_yaml('../yaml')
+    classes = []
+    for t in m.types:
+        if type(t) == model.EnumType:
+            write_enum(t, buffer)
+        if type(t) == model.ClassType:
+            classes.append(t)
+    buffer.write('### Class types ###\n\n')
+    write_classes(classes, buffer)
+    print(buffer.getvalue())
+
+
+def write_enum(t, buffer):
+    buffer.write(':%s a rdfs:Class;\n' % t.name)
+    buffer.write('  rdfs:subClassOf :Enumeration;\n')
+    buffer.write('  rdfs:comment "%s"\n' % t.doc)
+    buffer.write('  .\n\n')
+    for item in t.items:
+        buffer.write(':%s a rdfs:Class;\n' % item.name)
+        buffer.write('  rdfs:subClassOf :%s;\n' % t.name)
+        buffer.write('  .\n\n')
+
+
+def write_classes(classes, buffer):
+    for t in classes:
+        buffer.write(':%s a rdfs:Class;\n' % t.name)
+        if not t.super_class is None:
+            buffer.write('  rdfs:subClassOf :%s;\n' % t.super_class)
+        buffer.write('  rdfs:comment "%s"\n' % t.doc)
+        buffer.write('  .\n\n')
 
 
 def get_header():
@@ -10,18 +46,14 @@ def get_header():
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
 @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
 
+### Enumeration types ###
+
 :Enumeration a rdfs:Class;
-  rdfs:comment "The super class for all enumeration types."
+  rdfs:comment "The super-class of all enumeration types."
   .
+
 """
 
-m = model.Model()
-m.load_yaml('../yaml')
 
-text_model = get_header()
-
-
-
-print(text_model)
-
-
+if __name__ == '__main__':
+    main()
