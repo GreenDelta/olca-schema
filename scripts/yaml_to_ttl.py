5a114be6aa76a49f6d2febc92a9f618e3a42d6b2
msrocka <michael.srocka@gmail.com>
2014-11-18 14:37:24 +0100

    included properties in conversion to turtle

--- scripts/yaml_to_ttl.py
+++ scripts/yaml_to_ttl.py
@@ -8,14 +8,18 @@ def main():
     buffer.write(get_header())
     m = model.Model.load_yaml('../yaml')
     classes = []
+    properties = {}
     for t in m.types:
         if type(t) == model.EnumType:
             write_enum(t, buffer)
         if type(t) == model.ClassType:
             classes.append(t)
+            collect_properties(t, properties)
     buffer.write('### Class types ###\n\n')
     write_classes(classes, buffer)
-    print(buffer.getvalue())
+    write_properties(properties, buffer)
+    with open('../generated/schema.ttl', 'w') as f:
+        f.write(buffer.getvalue())
 
 
 def write_enum(t, buffer):
@@ -29,6 +33,15 @@ def write_enum(t, buffer):
         buffer.write('  .\n\n')
 
 
+def collect_properties(t, properties):
+    for prop in t.properties:
+        if not prop.name in properties:
+            properties[prop.name] = (prop, [t])
+        else:
+            entry = properties[prop.name]
+            entry[1].append(t)
+
+
 def write_classes(classes, buffer):
     for t in classes:
         buffer.write(':%s a rdfs:Class;\n' % t.name)
@@ -36,6 +49,43 @@ def write_classes(classes, buffer):
             buffer.write('  rdfs:subClassOf :%s;\n' % t.super_class)
         buffer.write('  rdfs:comment "%s"\n' % t.doc)
         buffer.write('  .\n\n')
+
+
+def write_properties(properties, buffer):
+    for name in properties:
+        entry = properties[name]
+        domain = get_domain(entry[1])
+        prop = entry[0]
+        rdf_range = get_range(prop)
+        buffer.write(':%s a rdf:Property;\n' % name)
+        buffer.write('  rdfs:domain %s;\n' % domain)
+        buffer.write('  rdfs:range %s;\n' % rdf_range)
+        if len(entry[1]) == 1 and not prop.doc is None:
+            buffer.write('  rdfs:comment "%s"\n' % prop.doc)
+        buffer.write('  .\n\n')
+
+
+def get_domain(class_list):
+    if len(class_list) == 1:
+        return ':' + class_list[0].name
+    text = '[ a owl:Class; owl:unionOf ('
+    i = 0
+    for c in class_list:
+        i += 1
+        text += ':' + c.name
+        if i < len(class_list):
+            text += ' '
+    return text + ')]'
+
+
+def get_range(prop):
+    t = prop.field_type
+    if t.startswith('List['):
+        return 'rdf:List'
+    if t[0].isupper():
+        return ':' + t
+    else:
+        return 'xsd:' + t
 
 
 def get_header():
