49e3255c0fd3479f84d1bca85a07438d03de63df
msrocka <michael.srocka@gmail.com>
2022-08-16 10:42:49 +0200

    improved proto generation
    * use index numbers from schema definition
    * handle `@id` and `@type` fields for `Ref`, `RefEntity`, and `RootEntity`
    * only allow `RootEntity` classes as `ProtoType`

--- osch/proto.go
+++ osch/proto.go
@@ -35,31 +35,6 @@ const BytesHint = `  // When we map to the bytes type it means that we have no m
 `
 
 const FileFooter = `
-
-// We map the openLCA ModelType to this enumeration type because it is only
-// used in the categories. In order to be compatible with the JSON-LD '@type'
-// field we use the ProtoType enumeration type
-enum ProtoCategoryType {
-
-  UNDEFINED_CATEGORY_TYPE = 0;
-  ACTOR = 1;
-  CURRENCY = 3;
-  DQ_SYSTEM = 4;
-  FLOW = 5;
-  FLOW_PROPERTY = 6;
-  IMPACT_CATEGORY = 7;
-  IMPACT_METHOD = 8;
-  LOCATION = 9;
-  PARAMETER = 11;
-  PROCESS = 12;
-  PRODUCT_SYSTEM = 13;
-  PROJECT = 14;
-  SOCIAL_INDICATOR = 15;
-  SOURCE = 16;
-  UNIT_GROUP = 18;
-	RESULT = 19;
-}
-
 // This enumeration type is added for compatibility with the @type attribute of
 // the openLCA JSON-LD format. In the proto messages we limit its usage to
 // instances of CategorizedEntity and Ref while it is allowed for every type in
@@ -68,24 +43,22 @@ enum ProtoCategoryType {
 enum ProtoType {
   Undefined = 0;
   Actor = 1;
-  Category = 2;
-  Currency = 3;
-  DQSystem = 4;
+  Currency = 2;
+  DQSystem = 3;
+  Epd = 4;
   Flow = 5;
   FlowProperty = 6;
   ImpactCategory = 7;
   ImpactMethod = 8;
   Location = 9;
-  NwSet = 10;
-  Parameter = 11;
-  Process = 12;
-  ProductSystem = 13;
-  Project = 14;
-	Result = 15;
-  SocialIndicator = 16;
-  Source = 17;
-  Unit = 18;
-  UnitGroup = 19;
+  Parameter = 10;
+  Process = 11;
+  ProductSystem = 12;
+  Project = 13;
+  Result = 14;
+  SocialIndicator = 15;
+  Source = 16;
+  UnitGroup = 17;
 }
 `
 
@@ -96,7 +69,7 @@ func generateProto(yaml *YamlModel) string {
 	// write the message and enumeration types
 	for _, typeDef := range yaml.Types {
 		switch typeDef.Name() {
-		case "Entity", "RootEntity", "CategorizedEntity":
+		case "Entity", "RefEntity", "RootEntity":
 			continue
 		}
 
@@ -108,7 +81,7 @@ func generateProto(yaml *YamlModel) string {
 				buff.WriteString(comment)
 			}
 			buff.WriteString("message Proto" + class.Name + " {\n\n")
-			writeProtoFields(class, &buff, yaml.TypeMap, 1)
+			writeProtoFields(class, &buff, yaml.TypeMap)
 			buff.WriteString("}\n\n")
 			continue
 		}
@@ -148,35 +121,34 @@ func generateProto(yaml *YamlModel) string {
 // Writes the fields of the given class to the given buffer. This function
 // climbs up the class hierarchy and inlines the fields of the corresponding
 // super classes (as there is no extension mechanism in proto3).
-func writeProtoFields(class *YamlClass, buff *bytes.Buffer, types map[string]*YamlType, offset int) int {
-	count := offset
+func writeProtoFields(class *YamlClass, buff *bytes.Buffer, types map[string]*YamlType) {
 
 	// write fields of super classes recursively
 	if class.SuperClass != "" {
 		super := types[class.SuperClass]
 		if super != nil && super.Class != nil {
-			count = writeProtoFields(super.Class, buff, types, offset)
+			writeProtoFields(super.Class, buff, types)
 		}
 	}
 
 	// @type field
-	if class.Name == "Ref" || class.Name == "CategorizedEntity" {
+	if class.Name == "Ref" || class.Name == "RootEntity" {
 		buff.WriteString("  // The type name of the respective entity.\n")
-		buff.WriteString("  ProtoType type = " + strconv.Itoa(count))
-		buff.WriteString(" [json_name = \"@type\"];\n\n")
-		count++
+		buff.WriteString("  ProtoType type = 1 [json_name = \"@type\"];\n\n")
 	}
 
-	// ID field
-	if class.Name == "RootEntity" {
+	// @id field
+	if class.Name == "Ref" || class.Name == "RefEntity" {
 		buff.WriteString("  // The reference ID (typically an UUID) of the entity.\n")
-		buff.WriteString("  string id = " + strconv.Itoa(count))
-		buff.WriteString(" [json_name = \"@id\"];\n\n")
-		count++
+		buff.WriteString("  string id = 2 [json_name = \"@id\"];\n\n")
 	}
 
 	// write fields
 	for _, field := range class.Props {
+
+		if strings.HasPrefix(field.Name, "@") {
+			continue
+		}
 
 		// field comment
 		comment := formatComment(field.Doc, "  ")
@@ -192,11 +164,9 @@ func writeProtoFields(class *YamlClass, buff *bytes.Buffer, types map[string]*Ya
 		}
 
 		buff.WriteString("  " + protoType + " " + protoField +
-			" = " + strconv.Itoa(count) + ";\n\n")
-		count++
+			" = " + strconv.Itoa(field.Index) + ";\n\n")
 	}
 
-	return count
 }
 
 // Maps the given olca-schema type to a corresponding proto3 type.
