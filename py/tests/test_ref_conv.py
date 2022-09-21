dd1070ba7cc5d1543b79386dde0a958efac37163
msrocka <michael.srocka@gmail.com>
2022-09-21 18:04:16 +0200

    uploaded v0.0.4

--- py/tests/test_ref_conv.py
+++ py/tests/test_ref_conv.py
@@ -8,3 +8,11 @@ class RefConvTest(unittest.TestCase):
         prop = lca.FlowProperty(unit_group=lca.Ref(id='abc'))
         prop = lca.FlowProperty.from_json(prop.to_json())
         self.assertEqual('abc', prop.unit_group.id)
+
+    def test_refs(self):
+        refs = [lca.Ref(id=str(i)) for i in range(0, 3)]
+        method = lca.ImpactMethod(impact_categories=refs)
+        method = lca.ImpactMethod.from_json(method.to_json())
+        for i in range(0, 3):
+            ref = method.impact_categories[i]
+            self.assertEqual(str(i), ref.id)
