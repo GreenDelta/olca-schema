02e10a22cebc0adfb3dbf0d001696d7a156349a7
msrocka <michael.srocka@gmail.com>
2022-09-23 17:46:36 +0200

    write proto modules for results

--- osch/proto.go
+++ osch/proto.go
@@ -2,12 +2,51 @@ package main
 
 import (
 	"bytes"
+	"log"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"unicode"
 )
+
+// BytesHint is a comment we add to fields with `bytes` as data type.
+const ProtoBytesHint = `  // When we map to the bytes type it means that we have no matching message
+  // type and just put the raw bytes into the field. This is specifically true
+  // for our geometry data of locations which cannot be translated to valid
+  // GeoJSON using Protocol Buffers (as they do not support arrays of arrays).
+  // To indicate that this is a different field than the field in the
+  // olca-schema definition, we append the _bytes suffix to the field name
+`
+
+const ProtoRootFooter = `// This enumeration type is added for compatibility with the @type attribute of
+// the openLCA JSON-LD format. In the proto messages we limit its usage to
+// instances of CategorizedEntity and Ref while it is allowed for every type in
+// the JSON-LD format. Thus, you should use ignoringUnknownFields flag when
+// parsing openLCA JSON-LD messages with the generated proto parsers.
+enum ProtoType {
+  Undefined = 0;
+  Actor = 1;
+  Currency = 2;
+  DQSystem = 3;
+  Epd = 4;
+  Flow = 5;
+  FlowProperty = 6;
+  ImpactCategory = 7;
+  ImpactMethod = 8;
+  Location = 9;
+  NwSet = 10;
+  Parameter = 11;
+  Process = 12;
+  ProductSystem = 13;
+  Project = 14;
+  Result = 15;
+  SocialIndicator = 16;
+  Source = 17;
+  Unit = 18;
+  UnitGroup = 19;
+}
+`
 
 type protoWriter struct {
 	buff  *bytes.Buffer
@@ -35,8 +74,8 @@ func writeProtos(args *args) {
 		if pack != "" {
 			fileName = "olca." + pack + ".proto"
 		}
-
 		outFile := filepath.Join(buildDir, fileName)
+		log.Println("write file:", fileName)
 		err = os.WriteFile(outFile, buff.Bytes(), os.ModePerm)
 		check(err, "failed to write to file", outFile)
 	}
@@ -44,6 +83,34 @@ func writeProtos(args *args) {
 
 func (w *protoWriter) writePack() {
 	w.writeFileHeader()
+
+	for _, t := range w.model.Types {
+		if t.Package != w.pack {
+			continue
+		}
+		if t.IsEnum() {
+			w.writeEnum(t.Enum)
+			w.writeln()
+		}
+		if t.IsClass() {
+			class := t.Class
+			comment := formatComment(class.Doc, "")
+			if comment != "" {
+				w.write(comment)
+			}
+			w.writeln("message Proto" + class.Name + " {")
+			w.writeln()
+			w.writeFieldsOf(class)
+			w.writeln("}")
+			w.writeln()
+			w.writeln()
+		}
+	}
+
+	if w.pack == "" {
+		w.write(ProtoRootFooter)
+	}
+
 }
 
 func (w *protoWriter) writeFileHeader() {
@@ -77,128 +144,60 @@ func (w *protoWriter) writeFileHeader() {
 	w.writeln()
 }
 
-// BytesHint is a comment we add to fields with `bytes` as data type.
-const BytesHint = `  // When we map to the bytes type it means that we have no matching message
-  // type and just put the raw bytes into the field. This is specifically true
-  // for our geometry data of locations which cannot be translated to valid
-  // GeoJSON using Protocol Buffers (as they do not support arrays of arrays).
-  // To indicate that this is a different field than the field in the
-  // olca-schema definition, we append the _bytes suffix to the field name
-`
-
-const FileFooter = `
-// This enumeration type is added for compatibility with the @type attribute of
-// the openLCA JSON-LD format. In the proto messages we limit its usage to
-// instances of CategorizedEntity and Ref while it is allowed for every type in
-// the JSON-LD format. Thus, you should use ignoringUnknownFields flag when
-// parsing openLCA JSON-LD messages with the generated proto parsers.
-enum ProtoType {
-  Undefined = 0;
-  Actor = 1;
-  Currency = 2;
-  DQSystem = 3;
-  Epd = 4;
-  Flow = 5;
-  FlowProperty = 6;
-  ImpactCategory = 7;
-  ImpactMethod = 8;
-  Location = 9;
-  NwSet = 10;
-  Parameter = 11;
-  Process = 12;
-  ProductSystem = 13;
-  Project = 14;
-  Result = 15;
-  SocialIndicator = 16;
-  Source = 17;
-  Unit = 18;
-  UnitGroup = 19;
-}
-`
-
-func generateProto(yaml *YamlModel) string {
-	var buff bytes.Buffer
-	w := protoWriter{&buff, yaml}
-
-	w.writeFileHeader()
-
-	buff.WriteString(FileHeader)
-
-	// write the message and enumeration types
-	for _, typeDef := range yaml.Types {
-		switch typeDef.Name() {
-		case "Entity", "RefEntity", "RootEntity":
-			continue
-		}
-
-		// write a class definition
-		class := typeDef.Class
-		if class != nil {
-			comment := formatComment(class.Doc, "")
-			if comment != "" {
-				buff.WriteString(comment)
-			}
-			buff.WriteString("message Proto" + class.Name + " {\n\n")
-			writeProtoFields(class, &buff, yaml.TypeMap)
-			buff.WriteString("}\n\n")
-			continue
-		}
-
-		// write an enumeration
-		enum := typeDef.Enum
-		if enum != nil {
-			if enum.Name == "ModelType" {
-				continue
-			}
-			comment := formatComment(enum.Doc, "")
-			if comment != "" {
-				buff.WriteString(comment)
-			}
-
-			buff.WriteString("enum Proto" + enum.Name + " {\n\n")
-
-			buff.WriteString("  // This default option was added automatically\n")
-			buff.WriteString("  // and means that no values was set.\n")
-			buff.WriteString("  " + protoUndefinedOf(enum) + " = 0;\n\n")
-			for i, item := range enum.Items {
-				comment := formatComment(item.Doc, "  ")
-				if comment != "" {
-					buff.WriteString(comment)
-				}
-				buff.WriteString("  " + item.Name + " = " +
-					strconv.Itoa(i+1) + ";\n\n")
-			}
-			buff.WriteString("}\n\n")
-		}
+func (w *protoWriter) writeEnum(enum *YamlEnum) {
+	if enum.Name == "ModelType" {
+		return
 	}
+	comment := formatComment(enum.Doc, "")
+	if comment != "" {
+		w.write(comment)
+	}
+	w.writeln("enum Proto" + enum.Name + " {")
+	w.writeln()
 
-	buff.WriteString(FileFooter)
-	return buff.String()
+	// write the 'UNDEFINED_*' item
+	w.indln("// This default option was added automatically")
+	w.indln("// and means that no values was set.")
+	w.indln(protoUndefinedOf(enum) + " = 0;")
+	w.writeln()
+
+	for _, item := range enum.Items {
+		comment := formatComment(item.Doc, "  ")
+		if comment != "" {
+			w.write(comment)
+		}
+		w.indln(item.Name + " = " + strconv.Itoa(item.Index) + ";")
+		w.writeln()
+	}
+	w.writeln("}")
+	w.writeln()
 }
 
 // Writes the fields of the given class to the given buffer. This function
 // climbs up the class hierarchy and inlines the fields of the corresponding
 // super classes (as there is no extension mechanism in proto3).
-func writeProtoFields(class *YamlClass, buff *bytes.Buffer, types map[string]*YamlType) {
+func (w *protoWriter) writeFieldsOf(class *YamlClass) {
 
 	// write fields of super classes recursively
 	if class.SuperClass != "" {
-		super := types[class.SuperClass]
+		super := w.model.TypeMap[class.SuperClass]
 		if super != nil && super.Class != nil {
-			writeProtoFields(super.Class, buff, types)
+			w.writeFieldsOf(super.Class)
 		}
 	}
 
 	// @type field
 	if class.Name == "Ref" || class.Name == "RootEntity" {
-		buff.WriteString("  // The type name of the respective entity.\n")
-		buff.WriteString("  ProtoType type = 1 [json_name = \"@type\"];\n\n")
+		w.indln("// The type name of the respective entity.")
+		w.indln("ProtoType type = 1 [json_name = \"@type\"];")
+		w.writeln()
 	}
 
 	// @id field
-	if class.Name == "RefEntity" {
-		buff.WriteString("  // The reference ID (typically an UUID) of the entity.\n")
-		buff.WriteString("  string id = 2 [json_name = \"@id\"];\n\n")
+	if class.Name == "Ref" || class.Name == "RefEntity" {
+		w.indln("// The reference ID (typically an UUID) of the entity.")
+		w.indln("string id = 2 [json_name = \"@id\"];")
+		w.writeln()
 	}
 
 	// write fields
@@ -211,22 +210,23 @@ func writeProtoFields(class *YamlClass, buff *bytes.Buffer, types map[string]*Ya
 		// field comment
 		comment := formatComment(field.Doc, "  ")
 		if comment != "" {
-			buff.WriteString(comment)
+			w.write(comment)
 		}
 
 		protoType := toProtoType(field.Type)
 		protoField := toSnakeCase(field.Name)
 		if protoType == "bytes" {
-			buff.WriteString(BytesHint)
+			w.write(ProtoBytesHint)
 			protoField += "_bytes"
 		}
 
-		buff.WriteString("  ")
+		w.write("  ")
 		if field.IsOptional {
-			buff.WriteString("optional ")
+			w.write("optional ")
 		}
-		buff.WriteString(protoType + " " + protoField +
-			" = " + strconv.Itoa(field.Index) + ";\n\n")
+		w.writeln(protoType + " " + protoField +
+			" = " + strconv.Itoa(field.Index) + ";")
+		w.writeln()
 	}
 
 }
@@ -283,4 +283,8 @@ func (w *protoWriter) writeln(xs ...string) {
 		w.buff.WriteString(x)
 	}
 	w.buff.WriteRune('\n')
+}
+
+func (w *protoWriter) write(s string) {
+	w.buff.WriteString(s)
 }
